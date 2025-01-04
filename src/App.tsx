76d9916f736e648ffd39281a2868f39ca68c5c28
madhavigiduthuri <madhavi.giduthuri@gmail.com>
2025-01-04 15:31:09 +0530

    added a small fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-orange-300 h-screen">
-      <Navbar />
+    <div className="bg-orange-300 min-h-screen flex flex-col justify-center items-center pb-20">
+      <div className="bg-blue-950 h-[75vh] w-4/5">
+        <Navbar />
+      </div>
+
+      <div className="bg-blue-950 h-[90vh] w-4/5 mt-32"></div>
     </div>
   );
 }
